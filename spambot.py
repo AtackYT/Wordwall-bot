3291c6dd35a81a82cd1e5e74f68c39a0ca75c212
SleepyJesper <87932425+AtackYT@users.noreply.github.com>
2022-11-24 18:57:34 +0100

    Add files via upload

---
+++ spambot.py
@@ -0,0 +1,69 @@
+import time
+import webbrowser
+
+print('██     ██  ██████  ██████  ██      ██████   ██     ██  █████  ██      ██          ██████   ██████  ████████ ')
+print('██     ██ ██    ██ ██   ██ ██      ██   ██  ██     ██ ██   ██ ██      ██          ██   ██ ██    ██    ██')
+print('██  █  ██ ██    ██ ██████  ██      ██   ██  ██  █  ██ ███████ ██      ██          ██████  ██    ██    ██  ')
+print('██ ███ ██ ██    ██ ██   ██ ██      ██   ██  ██ ███ ██ ██   ██ ██      ██          ██   ██ ██    ██    ██  ')
+print(' ███ ███   ██████  ██   ██ ███████ ██████    ███ ███  ██   ██ ███████ ███████     ██████   ██████     ██   ')
+print('')
+print('████████████████████████████████████████████████████████████████████████████████████████████████████████████')
+
+game = input('Input the game type:')
+
+code = input('Iput the game id:')
+
+prefix = input('Enter bot prefix:')
+
+if game == 'maze':
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '1&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '2&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '3&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '4&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '5&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '6&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '7&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '8&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '9&mode=1&activityId=' + code + '&templateId=49')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '10&mode=1&activityId=' + code + '&templateId=49')
+
+elif game == 'missing word':
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '1&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '2&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '3&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '4&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '5&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '6&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '7&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '8&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '9&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    webbrowser.open('https://wordwall.net/leaderboardajax/addentry?score=999999999&time=999999999&name=' + prefix + '10&mode=1&activityId=' + code + '&templateId=36')
+    time.sleep (0.1)
+    
+
+else:
+    print('invalid input')
+
+time.sleep (2)
+
+print("done")
+
+input('Press any key to exit:')
